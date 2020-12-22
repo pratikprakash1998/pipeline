68bacb6543f25b60808532b87487264a2d6ccbda
PR20167953 <PR20167953@APL-G2QB403>
2020-12-22 12:37:25 +0530

    pipeline

---
+++ demo2-master/demo1/src/main/java/demo1/App.java
@@ -0,0 +1,17 @@
+package demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    	
+       // Calc c=new Calc();
+       // logger.log("My Message");
+        //System.out.println("sum:"+c.add(2, 3));
+    }
+}
